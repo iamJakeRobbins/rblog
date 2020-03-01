d249ad1a64c001f2c734b029613373078d7acabb
Jake Robbins <jrobbinsd@gmail.com>
2020-03-01 10:37:32 -0700

    import landing component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Carousel from "./MainComponents/Carousel";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
       </header>
+      <body>
+      <h1>hello world</h1>
+      <Carousel />
+      </body>
     </div>
   );
 }
