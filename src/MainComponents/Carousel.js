f8a69a15fa9c68fe212672dee4edeb722971c46b
Jake Robbins <jrobbinsd@gmail.com>
2020-03-01 10:36:27 -0700

    component skeleton

---
+++ src/MainComponents/Carousel.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Carousel extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>init</h1>
+      </div>
+    )
+  }
+
+  constructor() {
+    super();
+    console.log('here')
+  }
+}
