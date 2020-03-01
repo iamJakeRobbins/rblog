ae34eef6cf459d164c836e53aa4de2e7f6dbca98
Jake Robbins <jrobbinsd@gmail.com>
2020-03-01 10:41:24 -0700

    header for wireframes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Carousel from "./MainComponents/Carousel";
+import HeaderComponent from "./MainComponents/HeaderComponent";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <HeaderComponent />
       </header>
       <body>
       <Carousel />
